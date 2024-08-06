e2ca034bf8fcde81afac877266d70dfcf4077d23
unknown <baraahedri69@gmail.com>
2024-08-06 17:42:49 +0100

    todo 1

---
+++ Trees/Trees I/Practice/Trees To Do 1/todo1.js
@@ -0,0 +1,97 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+class BST {
+    constructor() {
+        this.root = null;
+    }
+    // BST: ADD
+    add(value) {
+        if(this.root) {
+            var runner = this.root;
+            while(runner) {
+                if(value>runner.value) {
+                    if(runner.right){
+                        runner = runner.right;
+                    } else {
+                        runner.right = new Node(value);
+                        return this
+                    }
+                } else {
+                    if(runner.left){
+                        runner = runner.left;
+                    } else {
+                        runner.left = new Node(value);
+                        return this
+                    }
+                }
+            }
+        }
+        this.root = new Node(value);
+        return this
+    }
+    contains(value) {
+        var runner = this.root;
+        while (runner) {
+            if (value == runner.value) {
+                return true;
+            }
+            if (value < runner.value) {
+                if (!runner.left) {
+                    return false;
+                }
+                runner = runner.left;
+            } else {
+                if (!runner.right) {
+                    return false;
+                }
+                runner = runner.right;
+            }
+        }
+        return false;
+    }
+    min() {
+        var runner = this.root;
+        var min = this.root.value;
+        while(runner.left) {
+            if(runner.left.value < min) {
+                min = runner.left.value;
+            }
+            runner = runner.left;
+        }
+        return min
+    }
+    max() {
+        var runner = this.root;
+        var max = this.root.value;
+        while(runner.right) {
+            if(runner.right.value > max) {
+                max = runner.right.value;
+            }
+            runner = runner.right;
+        }
+        return max
+    }
+    size() {
+        if (this.root === null) {
+            return 0;
+        }
+        function sizeHelp(runner) {
+            if (!runner) {
+                return 0;
+            }
+            return 1 + sizeHelp(runner.left) + sizeHelp(runner.right);
+        }
+        return sizeHelp(this.root);
+    }
+    isEmpty() {
+        if(this.root) {
+            return false
+        }
+        return true
+    }
+}
